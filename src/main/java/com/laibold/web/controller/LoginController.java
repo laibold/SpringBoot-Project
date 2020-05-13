3d048316aa04c707676eaf0be8bbfaf061099b58
Marvin <marvin.laibold@yahoo.de>
2020-05-13 13:45:14 +0200

    Reset

---
+++ src/main/java/com/laibold/web/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.laibold.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+
+
+}
