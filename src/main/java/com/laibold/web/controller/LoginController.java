ea8cf3f53c926b1f464d1be959b575e47fddeab9
Marvin <marvin.laibold@yahoo.de>
2020-05-13 15:32:55 +0200

    Login

--- src/main/java/com/laibold/web/controller/LoginController.java
+++ src/main/java/com/laibold/web/controller/LoginController.java
@@ -1,12 +1,33 @@
 package com.laibold.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @GetMapping("")
+    public String loginGet(Model m) {
+        return "login";
+    }
 
+    @PostMapping("")
+    public String loginPost(Model m, @RequestParam String username, @RequestParam String password) {
+        String correctPassword = username + username.length();
+        if (!password.equals(correctPassword)) {
+            logger.error("Failed login attempt with user {} and password {}.", username, password);
+            String hint = "Hinweis: Das korrekte Passwort für " + username + " ist " + correctPassword;
+            m.addAttribute("passwordHint", hint);
+        }
+            return "login";
+    }
 
 }
---
+++ src/main/java/com/laibold/web/model/BratenDaten.java
@@ -0,0 +1,10 @@
+package com.laibold.web.model;
+
+import java.time.LocalDate;
+
+public class BratenDaten {
+    String name;
+    String abholort;
+    LocalDate haltbarBis;
+    String beschreibung;
+}
